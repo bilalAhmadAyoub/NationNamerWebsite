662eac06822362b32154978def271a823cdc844d
bilalAhmadAyoub <102704100+bilalAhmadAyoub@users.noreply.github.com>
2023-01-02 20:55:03 -0600

    Update game.js

--- game.js
+++ game.js
@@ -38,6 +38,7 @@ function readTextFile(file, arrayName) {
 
 //let's read in the region names, depending on which map we are using
 var pageName = window.location.pathname.split("/").pop();
+console.log("pathname is " + pathName);
 if(pageName == "index.html"){
     console.log("reached a");
     readTextFile('/regionLists/countryData.txt', globalCountryList);
