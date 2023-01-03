8d1670e9f35e62a862086074934285a59798741b
bilalAhmadAyoub <102704100+bilalAhmadAyoub@users.noreply.github.com>
2023-01-02 21:39:01 -0600

    Update game.js

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ var gameOver = false
 
 // read in the country names from countryData.txt and add them to globalCountryList array
 function readTextFile(file, arrayName) {
+    console.log("reached");
     let rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
     rawFile.onreadystatechange = function () {
@@ -32,9 +33,7 @@ function readTextFile(file, arrayName) {
 
 //let's read in the region names, depending on which map we are using
 var pageName = window.location.pathname.split("/").pop();
-if(pageName == "index.html"){
-    readTextFile('/regionLists/countryData.txt', globalCountryList);
-}
+if(pageName == "index.html") readTextFile('/regionLists/countryData.txt', globalCountryList);
 else if(pageName == "unitedStates.html") readTextFile('/regionLists/americanStatesData.txt', globalCountryList);
 else if(pageName == "centralAndSouthAmerica.html") {
     var unorderedCountries = [];
